631d2f28251f36073930fbb8c383335b6a150d68
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-22 13:25:41 +0100

    Loading routes lazily
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8138598?start=0

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { NavLink, Route, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
 	state = {
-		auth: false
+		auth: true
 	};
 
 	render() {
@@ -49,7 +51,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 				{/* The order here is important. The 1st one to match is triggered */}
 				<Switch>
-					{this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+					{this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
 					<Route path="/posts" component={Posts} />
 					<Route render={() => <h1>Not found</h1>} />
 					{/* <Redirect from="/" to="/posts" /> */}
