0e0ec2b07446cec541b0e2d405f003b56d6da17f
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-22 13:08:54 +0100

    Working with guards
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8138592?start=0

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,6 +6,10 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+	state = {
+		auth: false
+	};
+
 	render() {
 		return (
 			<div className="Blog">
@@ -45,7 +49,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 				{/* The order here is important. The 1st one to match is triggered */}
 				<Switch>
-					<Route path="/new-post" component={NewPost} />
+					{this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
 					<Route path="/posts" component={Posts} />
 					<Redirect from="/" to="/posts" />
 				</Switch>
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -10,6 +10,11 @@ class NewPost extends Component {
 		author: 'Max'
 	};
 
+	componentDidMount() {
+		// To check whether authenticated and can use this component.
+		// if unauth = this.props.history.replace('/posts');
+	}
+
 	postDataHandler = () => {
 		const data = {
 			title: this.state.title,
