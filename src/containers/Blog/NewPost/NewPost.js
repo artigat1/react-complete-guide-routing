91c1dca8c24b0ce2b4f592826ebf4124273d853f
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-22 13:04:01 +0100

    Using the history prop to redirect (replace)
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8138590?start=0
    
    This means the back button works as expected. Redirecting actually replaces the current page in the history, so the back button won't work as expected.

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -8,8 +7,7 @@ class NewPost extends Component {
 	state = {
 		title: '',
 		content: '',
-		author: 'Max',
-		submitted: false
+		author: 'Max'
 	};
 
 	postDataHandler = () => {
@@ -19,20 +17,13 @@ class NewPost extends Component {
 			author: this.state.author
 		};
 		axios.post('/posts', data).then((response) => {
-			console.log(response);
-			this.setState({ submitted: true });
+			this.props.history.push('/posts');
 		});
 	};
 
 	render() {
-		let redirect = undefined;
-		if (this.state.submitted) {
-			redirect = <Redirect to="/posts" />;
-		}
-
 		return (
 			<div className="NewPost">
-				{redirect}
 				<h1>Add a Post</h1>
 				<label>Title</label>
 				<input
